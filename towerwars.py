fef6f67d96dfd411fdfa8a0215ba0ceb1d91b513
Eric Sumner <kd5bjo@gmail.com>
2009-03-07 10:24:02 -0600

    Beginnings of an event system; attract mode
    I've established a grid to play on, and can paint the individual cells of that
    grid whatever color I want.  Also, it quits cleanly now and can translate mouse
    coordinates into grid coordinates.  I don't know if I actually want to use the
    mouse in the game, but it was a good test to make sure I understood the
    coordinate system.

--- towerwars.py
+++ towerwars.py
@@ -21,17 +21,36 @@
 import pygame, sys, os, traceback
 from pygame.locals import *
 from select import select
-
-import gc
-gc.disable()
+import random
 
 pygame.init()
-pygame.display.set_mode((512, 768))
+pygame.display.set_mode((512, 768), pygame.DOUBLEBUF)
 
 FPS = 30
 next_frame_time = 0
 frameno = 0
 fds = []
+event_delay = 5 #frames
+remote_frame_offset = 0
+
+class EventManager:
+    def __init__(self):
+        self.cache = {}
+
+    def getevents(self):
+        if (frameno - 1) in self.cache:
+            del self.cache[frameno - 1]
+        return self.cache.get(frameno, [])
+    
+    def add_local_event(self, event):
+        if (frameno + event_delay) not in self.cache:
+            self.cache[frameno + event_delay] = []
+        self.cache[frameno + event_delay].append(event)
+
+    def add_remote_event(self, event, time):
+        if (time + remote_frame_offset) not in self.cache:
+            self.cache[time + remote_frame_offset] = []
+        self.cache[time + remote_frame_offset].append(event)
 
 class SelectSocket:
     def __init__(self, file):
@@ -69,48 +88,94 @@ class SelectSocket:
 class Log(SelectSocket):
     def __init__(self):
         SelectSocket.__init__(self, sys.stdout)
-        self.verbosity = 4 # Trace execution
+        self.verbosity = 5 # Trace execution
         self.desc = {0: 'FATAL', 1: 'ERROR', 2: 'WARN', 3:'INFO', 4:'DEBUG', 5:'TRACE'}
         self.msg(3, 'Logging', 'Log opened')
 
     def msg(self, level, label, message, **kwargs):
         if level > self.verbosity: return
         self.outbuf += '%8d %5s %17s: %s\t%s\n' % (frameno, self.desc.get(level, level), label, message, kwargs)
-    
+
 log = Log()
 
 fds.append(log)
 
-color = 0
+# Game State
+class Playfield:
+    def __init__(self):
+       self.occupancy = [[0]*32 for x in range(48)]
 
+    def render(self, (offy, offx)):
+        for y, row in enumerate(self.occupancy):
+            for x, val in enumerate(row):
+                cell = pygame.Rect(16*x+offx, 16*y+offy, 16, 16)
+                pygame.display.get_surface().fill((val,val,val), cell)
+
+    def mutate(self):
+        for y, row in enumerate(self.occupancy):
+            for x, val in enumerate(row):
+                if val: row[x] = val - 1
+
+        x = random.randint(0,31)
+        y = random.randint(0,47)
+        val = random.randint(0,255)
+        self.occupancy[y][x] = val
+
+    def clear(self, (x, y)):
+        self.occupancy[y/16][x/16] = 0
+
+playfield = Playfield()
+
+# Game Display
+def render_frame():
+    pygame.display.get_surface().fill((0,0,0))
+    playfield.render((0,0))
+    pygame.display.flip()
+
+# Game Logic, called for every frame, even dropped ones
+def process_updates():
+    for ev in pygame.event.get():
+        log.msg(5, 'PygameEvent', ev)
+        if ev.type == MOUSEMOTION: playfield.clear(ev.pos)
+        if ev.type == KEYDOWN and ev.key == pygame.K_q: sys.exit(0)
+    playfield.mutate()
+    
+# Event Loop
 next_frame_time = pygame.time.get_ticks()
-while True:
-    next_frame_time += 1000/FPS
-    wait_interval = next_frame_time - pygame.time.get_ticks()
-    frameno += 1
-    if wait_interval<0:
-        log.msg(2, 'EventLoop', 'Dropping Frame', interval=wait_interval)
-    else:
-        pygame.display.get_surface().fill((color, color, color))
-        pygame.display.update()
-    log.msg(5, 'EventLoop', 'Waiting', interval=wait_interval)
-    while wait_interval > 0:
-        try:
-            if [f.rtr() or f.rts() for f in fds]:
-                rd,wr,ex = select( [f for f in fds if f.rtr()],
-                                   [f for f in fds if f.rts()],
-                                   [], wait_interval/1000.)
-                for s in wr:
-                    s.send()
-                for s in rd:
-                    s.recv()
-            else:
-                pygame.time.wait(wait_interval)
-        except Exception, e:
-            log.msg(1, 'EventLoop', traceback.format_exception_only(type(e),e)[-1].strip())
-            traceback.print_exc()
+try:
+    while True:
+        next_frame_time += 1000/FPS
         wait_interval = next_frame_time - pygame.time.get_ticks()
-
-    #Game Logic
-    color += 1
-    if color > 255: color = 0
+        frameno += 1
+        if wait_interval<0:
+            log.msg(2, 'EventLoop', 'Dropping Frame', interval=wait_interval)
+        else:
+            try: render_frame()
+            except Exception, e:
+                log.msg(1, 'RenderFrame', traceback.format_exception_only(type(e),e)[-1].strip())
+                traceback.print_exc()
+        log.msg(5, 'EventLoop', 'Waiting', interval=wait_interval)
+        while wait_interval > 0:
+            try:
+                if [f.rtr() or f.rts() for f in fds]:
+                    rd,wr,ex = select( [f for f in fds if f.rtr()],
+                                       [f for f in fds if f.rts()],
+                                       [], wait_interval/1000.)
+                    for s in wr:
+                        s.send()
+                    for s in rd:
+                        s.recv()
+                else:
+                    pygame.time.wait(wait_interval)
+            except Exception, e:
+                log.msg(1, 'EventLoop', traceback.format_exception_only(type(e),e)[-1].strip())
+                traceback.print_exc()
+            wait_interval = next_frame_time - pygame.time.get_ticks()
+    
+        #Game Logic
+        try: process_updates()
+        except Exception, e:
+            log.msg(1, 'ProcessUpdates', traceback.format_exception_only(type(e),e)[-1].strip())
+            traceback.print_exc()
+except BaseException, e:
+    log.flush()
