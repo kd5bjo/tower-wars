9147b13937d736477a71eaa05b600fa2e991fbca
Eric Sumner <kd5bjo@gmail.com>
2009-03-07 17:02:01 -0600

    Networking!
    I really spent a lot of time on this; I hope it's as robust as I think it is.
    Once per frame, each side sends a ping packet that includes both the local
    frame number and the most-recently-seen remote frame number.  After 30 of
    these have been exchanged (1sec), the server decides on a correlation between
    the two sequences of frame numbers, and both sides use that to figure out
    when things should happen.  All events are fired at the same time on both
    computers, in a deterministic order.  To do this, locally-generated events
    are delayed 5 frames before they take effect.  This scheme should be able to
    accomodate up to 10 frames of round-trip ping time (330 ms).  This can be
    increased if the ping times over the general internet are higher than this.

--- towerwars.py
+++ towerwars.py
@@ -22,7 +22,9 @@ import pygame, sys, os, traceback
 from pygame.locals import *
 from select import select
 from optparse import OptionParser
-import random
+import random, socket
+
+random_seed = random.getrandbits(32)
 
 option_parser = OptionParser()
 
@@ -35,7 +37,7 @@ option_parser.add_option('-l', '--logfile', action='store', type='string', dest=
 
 # Networking Options
 
-option_parser.add_option('-s', '--server', action='store_true', dest='server', default='False', help='Run as a server.')
+option_parser.add_option('-s', '--server', action='store_true', dest='server', default=False, help='Run as a server.')
 option_parser.add_option('-c', '--client', action='store', dest='ip', type='string', default='0.0.0.0', help='Run as a client, connecting to the server at IP.')
 option_parser.add_option('-p', '--port', action='store', dest='port', type='int', default='4242', help='Port number for TCP connections.')
 
@@ -47,40 +49,20 @@ pygame.display.set_mode((512, 768), pygame.DOUBLEBUF)
 FPS = 30
 next_frame_time = 0
 frameno = 0
-fds = []
+fds = set()
 event_delay = 5 #frames
-remote_frame_offset = 0
-
-class EventManager:
-    def __init__(self):
-        self.cache = {}
-
-    def getevents(self):
-        if (frameno - 1) in self.cache:
-            del self.cache[frameno - 1]
-        return self.cache.get(frameno, [])
-    
-    def add_local_event(self, event):
-        if (frameno + event_delay) not in self.cache:
-            self.cache[frameno + event_delay] = []
-        self.cache[frameno + event_delay].append(event)
-
-    def add_remote_event(self, event, time):
-        if (time + remote_frame_offset) not in self.cache:
-            self.cache[time + remote_frame_offset] = []
-        self.cache[time + remote_frame_offset].append(event)
-
-event_manager = EventManager()
 
 class SelectSocket:
     def __init__(self, file):
         self.inbuf  = ''
         self.outbuf = ''
         self.open = True
+        self.fileobj = None
         if isinstance(file, int):
             self.fd = file
         elif hasattr(file, 'fileno'):
             self.fd = file.fileno()
+            self.fileobj = file
         else:
             self.fd = os.open(file, os.O_WRONLY | os.O_APPEND)
 
@@ -124,8 +106,121 @@ class Log(SelectSocket):
 
 log = Log()
 
-fds.append(log)
+fds.add(log)
 
+class EventManager:
+    def __init__(self):
+        self.cache = {}
+        self.state = 'Standalone'
+        self.remote_frame = 0
+        self.remote_frame_offset = None
+        self.rtts = []
+        self.handlers = {}
+
+        if options.server:
+            self.state = 'Listening'
+            self.socket = socket.socket()
+            self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            self.socket.bind(('0.0.0.0', options.port))
+            self.socket.listen(-1)
+            log.msg(3, 'Network', 'Listening', port=options.port)
+            class ListenSocket:
+                def fileno(skt):
+                    return self.socket.fileno()
+                def rts(skt):
+                    return False
+                def rtr(skt):
+                    return True
+                def recv(skt):
+                    newSocket, remoteAddr = self.socket.accept()
+                    self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                    self.state = 'Connected'
+                    log.msg(3, 'Network', 'Connected', ip=remoteAddr[0], port=remoteAddr[1])
+                    fds.remove(skt)
+                    self.socket = SelectSocket(newSocket)
+                    fds.add(self.socket)
+            fds.add(ListenSocket())
+        elif options.ip != '0.0.0.0':
+            try:
+                self.socket = socket.socket()
+                self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                self.socket.connect((options.ip, options.port))
+                log.msg(3, 'Network', 'Connected', ip=self.socket.getpeername()[0], port=self.socket.getpeername()[1])
+                self.state = 'Connected'
+            except:
+               sys.exit(1)
+            self.socket = SelectSocket(self.socket)
+            fds.add(self.socket)
+                
+    def run_events(self):
+        if (frameno - 1) in self.cache:
+            del self.cache[frameno - 1]
+        if self.state in ('Connected', 'Synchronized'):
+            remote_events = self.socket.inbuf.splitlines(True)
+            if remote_events and remote_events[-1][-1] not in '\r\n':
+                self.socket.inbuf = remote_events[-1]
+                remote_events = remote_events[:-1]
+            else:
+                self.socket.inbuf = ''
+            for ev in remote_events:
+                timestamp, args = ev.split(None, 1)
+                args = args.split()
+                timestamp = int(timestamp)
+                log.msg(5, 'RemoteEvent', args[0], remote=timestamp, args=args[1:])
+                if self.state == 'Synchronized':
+                    if args[0] not in ('ping', 'synchronize'):
+                        self.add_remote_event(args, timestamp)
+                elif self.state == 'Connected':
+                    assert args[0] in ('ping', 'synchronize')
+                    if args[0] == 'ping':
+                        self.remote_frame = timestamp
+                        if int(args[1]) != 0:
+                            self.rtts.append(frameno - int(args[1]))
+                            log.msg(5, 'Clock', 'AddedRTT', start=int(args[1]), value=self.rtts[-1], remote=timestamp)
+                            if options.server and len(self.rtts) >= 30:
+                                self.state = 'Synchronized'
+                                rtt = sum(self.rtts)/(2*len(self.rtts))
+                                self.remote_frame_offset = frameno - timestamp + rtt
+                                log.msg(3, 'Clock', 'Synchronized', rtt=rtt, frame_offset=self.remote_frame_offset)
+                                self.socket.outbuf += '%d synchronize %d\n' % \
+                                    (frameno, frameno-self.remote_frame_offset)
+                                self.add_event('randomize', random_seed)
+                                self.add_event('reset')
+                    if args[0] == 'synchronize':
+                        rtt = sum(self.rtts)/(2*len(self.rtts))
+                        self.state = 'Synchronized'
+                        self.remote_frame_offset = int(args[1]) - timestamp
+                        log.msg(3, 'Clock', 'Synchronized', rtt=rtt, frame_offset=self.remote_frame_offset)
+            if self.state == 'Connected':
+                self.socket.outbuf += '%d ping %d\n' % (frameno, self.remote_frame)
+        add_local_events(self)
+    
+        precedence = ['remote', 'local']
+        if options.server: precedence = ['local', 'remote']
+
+        for p in precedence:
+            for ev in (e for e in self.cache.get(frameno, []) if e[0] == p):
+                if ev[1] in self.handlers:
+                    log.msg(4, 'Event', ev[1], args=ev[2:])
+                    self.handlers[ev[1]](*ev[2:])
+        self.handlers['tick']()
+
+    def add_event(self, *event):
+        if (frameno + event_delay) not in self.cache:
+            self.cache[frameno + event_delay] = []
+        event = [str(e) for e in event]
+        self.cache[frameno + event_delay].append(['local']+event)
+        if self.state == 'Synchronized':
+            self.socket.outbuf += '%d %s\n' % (frameno + event_delay, ' '.join(event))
+
+    def add_remote_event(self, event, time):
+        time += self.remote_frame_offset
+        assert time >= frameno
+        if time not in self.cache:
+            self.cache[time] = []
+        self.cache[time].append(['remote']+event)
+
+event_manager = EventManager()
 # Game State
 class Playfield:
     def __init__(self):
@@ -153,7 +248,11 @@ class Playfield:
             self.occupancy[y/16][x/16] = 0
             log.msg(4, 'Playfield', 'Clear', cell=(y,x))
 
-playfield = Playfield()
+def resetGame():
+    global playfield
+    playfield = Playfield()
+
+resetGame()
 
 # Game Display
 def render_frame():
@@ -161,17 +260,21 @@ def render_frame():
     playfield.render((0,0))
     pygame.display.flip()
 
-# Game Logic, called for every frame, even dropped ones
-def process_updates():
+# Local Controls
+def add_local_events(evmanager):
     for ev in pygame.event.get():
         log.msg(5, 'PygameEvent', pygame.event.event_name(ev.type), **ev.dict)
-        if ev.type == MOUSEMOTION:
-            event_manager.add_local_event((playfield.clear, ev.pos))
-        if ev.type == KEYDOWN and ev.key == pygame.K_q: sys.exit(0)
-    for ev in event_manager.getevents():
-        ev[0](*ev[1:])
-    playfield.mutate()
+        if ev.type == MOUSEBUTTONDOWN:
+            evmanager.add_event('clear', ev.pos[0], ev.pos[1])
+        if ev.type == KEYDOWN and ev.key == pygame.K_q:
+            evmanager.add_event('quit')
     
+event_manager.handlers['randomize'] = lambda x: random.seed(int(x))
+event_manager.handlers['reset'] = resetGame
+event_manager.handlers['clear'] = lambda x,y: playfield.clear((int(x), int(y)))
+event_manager.handlers['tick'] = lambda: playfield.mutate()
+event_manager.handlers['quit'] = lambda: sys.exit(0)
+
 # Event Loop
 next_frame_time = pygame.time.get_ticks()
 try:
@@ -205,9 +308,9 @@ try:
             wait_interval = next_frame_time - pygame.time.get_ticks()
     
         #Game Logic
-        try: process_updates()
+        try: event_manager.run_events()
         except Exception, e:
-            log.msg(1, 'ProcessUpdates', traceback.format_exception_only(type(e),e)[-1].strip())
+            log.msg(1, 'RunEvents', traceback.format_exception_only(type(e),e)[-1].strip())
             traceback.print_exc()
 except BaseException, e:
     log.flush()
